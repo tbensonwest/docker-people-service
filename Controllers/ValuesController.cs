651de683176382cce6113dabf9b4ab52b3955478
unknown <Tarran.West@ASGLH-WL-06647.kingsway.asos.com>
2016-02-16 14:36:34 +0000

    Second commit

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace people_service.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Fuck", "Yeah!" };
         }
 
         // GET api/values/5
